574984eb2daa9cd35f8f40ada3bac8c54c2181c8
Carlos Arturo <zs20006730@estudiantes.uv.mx>
2023-05-04 05:48:34 -0600

    project

--- src/components/insta-list.component.js
+++ src/components/insta-list.component.js
@@ -1,100 +1,102 @@
 import React, { Component } from "react";
 import photosDataService from "../services/insta.service";
-
+import '../styles/cards.css';
+import '../styles/player.css'
 import Insta from "./insta.component";
 
 export default class PhotosList extends Component {
-  constructor(props) {
-    super(props);
-    this.refreshList = this.refreshList.bind(this);
-    this.setActiveTutorial = this.setActiveTutorial.bind(this);
-    this.onDataChange = this.onDataChange.bind(this);
-
-    this.state = {
-      tutorials: [],
-      currentTutorial: null,
-      currentIndex: -1,
-    };
-
-    this.unsubscribe = undefined;
-  }
-
-  componentDidMount() {
-    this.unsubscribe = photosDataService.getAll().orderBy("title", "asc").onSnapshot(this.onDataChange);
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  onDataChange(items) {
-    let tutorials = [];
-
-    items.forEach((item) => {
-      let id = item.id;
-      let data = item.data();
-      tutorials.push({
-        id: id,
-        title: data.title,
-        description: data.description,
-        published: data.published,
-      });
-    });
-
-    this.setState({
-      tutorials: tutorials,
-    });
-  }
-
-  refreshList() {
-    this.setState({
-      currentTutorial: null,
-      currentIndex: -1,
-    });
-  }
-
-  setActiveTutorial(tutorial, index) {
-    this.setState({
-      currentTutorial: tutorial,
-      currentIndex: index,
-    });
-  }
-
-  render() {
-          const { tutorials, currentTutorial, currentIndex } = this.state;
-      
-          return (
-            <div className="list row">
-              <div className="col-md-6">
-                <h4>Tutorials List</h4>
-      
-                <ul className="list-group">
-                  {tutorials &&
-                    tutorials.map((tutorial, index) => (
-                      <li
-                        className={ "list-group-item " + (index === currentIndex ? "active" : "") }
-                        onClick={() => this.setActiveTutorial(tutorial, index)}
-                        key={index}
-                      >
-                        {tutorial.title}
-                      </li>
-                    ))}
-                </ul>
-              </div>
-              <div className="col-md-6">
+    constructor(props) {
+        super(props);
+        this.refreshList = this.refreshList.bind(this);
+        this.setActiveTutorial = this.setActiveTutorial.bind(this);
+        this.onDataChange = this.onDataChange.bind(this);
+    
+        this.state = {
+            tutorials: [],
+            currentTutorial: null,
+            currentIndex: -1,
+        };
+    
+        this.unsubscribe = undefined;
+        }
+    
+        componentDidMount() {
+        this.unsubscribe = photosDataService.getAll().orderBy("title", "asc").onSnapshot(this.onDataChange);
+        }
+    
+        componentWillUnmount() {
+        this.unsubscribe();
+        }
+    
+        onDataChange(items) {
+        let tutorials = [];
+    
+        items.forEach((item) => {
+            let id = item.id;
+            let data = item.data();
+            tutorials.push({
+            id: id,
+            title: data.title,
+            description: data.description,
+            published: data.published,
+            url: data.url,
+            });
+        });
+    
+        this.setState({
+            tutorials: tutorials,
+        });
+        }
+    
+        refreshList() {
+        this.setState({
+          currentTutorial: null,
+            currentIndex: -1,
+        });
+        }
+    
+        setActiveTutorial = (tutorial) => {
+        this.setState({
+          currentTutorial: tutorial,
+        });
+        };
+    
+        
+    
+        render() {
+        const { tutorials, currentTutorial } = this.state;
+    
+        return (
+            <><div className="contenedorDeMiniatura">
+            {tutorials.map((tutorial) => (
+                <div className="miniatura" key={tutorial.title}>
+                <div
+                    className="card"
+                    onClick={() => {
+                    console.log("La tarjeta ha sido clickeada");
+                    this.setActiveTutorial(tutorial);
+                    }}
+                >
+                    <div className="image-container">
+                    <img className="card-img-top image" src={tutorial.url} alt=""></img>
+                    </div>
+                </div>
+                <h5 className="title">{tutorial.title}</h5>
+                </div>
+            ))}
+            </div>
+            <div className="mostrarImagen">
                 {currentTutorial ? (
-                  <Insta
+                <Insta
                     tutorial={currentTutorial}
                     refreshList={this.refreshList}
-                  />
+                    key={currentTutorial.id}
+                />
                 ) : (
-                    <div>
-                        <br />
-                        <p>Please click on a Tutorial...</p>
-                    </div>
-                )}
+                <div>
                 </div>
-            </div>
-            );
-        }
-    }
+                )}
+            </div></>
+        );
+      }
+}
