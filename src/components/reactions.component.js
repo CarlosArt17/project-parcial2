f4856671a958ca2421667150e0ef445cd535f9ba
Carlos Arturo <zs20006730@estudiantes.uv.mx>
2023-05-18 15:38:10 -0600

    cambios finales

--- src/components/reactions.component.js
+++ src/components/reactions.component.js
@@ -4,7 +4,12 @@ import '../styles/photo.css'
 function ReactionsComponent() {
   return (
     <div className="reactions">
+      <div className="reaction reaction-like"></div>
       <div className="reaction reaction-love"></div>
+      <div className="reaction reaction-important"></div>
+      <div className="reaction reaction-funny"></div>
+      <div className="reaction reaction-anger"></div>
+      <div className="reaction reaction-sad"></div>
     </div>
   );
 }
