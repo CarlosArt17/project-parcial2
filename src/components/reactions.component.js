457885e814896c6e62fceafadd94515fdf922718
SrCarlangas69 <zs20006730@estudiantes.uv.mx>
2023-05-23 10:20:22 -0600

    update

--- src/components/reactions.component.js
+++ src/components/reactions.component.js
@@ -2,38 +2,64 @@ import React from "react";
 import '../styles/photo.css'
 import kafkaService from "../services/kafka.service";
 
-function saveLike(e, status) { 
-  let data = {
-    id: 0,
-    status: status
-  };
-  console.log(JSON.stringify(data));
-  kafkaService.reaction("i-love-adsoftsito");
-  e.preventDefault();
-}
-
-
-
-
-
 function ReactionsComponent() {
+  function saveLike(e, status) {
+  
+     let data = {
+       id: 0,
+       status: status
+     };
+  
+     console.log(JSON.stringify(data));
+  
+     KafkaService.reaction("i-love-adsoftsito");
+     e.preventDefault();
+ }
+
   return (
     <div className="reactions">
-      <div className="reaction reaction-like">
-      <button onClick={(e) => {
+      <div className="reaction reaction-like" onClick={(e) => {
                     e.preventDefault();
                     saveLike(e, 1)
                       
                     }
-                } >
-                 Love
-              </button>
-    </div>
-      <div className="reaction reaction-love"></div>
-      <div className="reaction reaction-important"></div>
-      <div className="reaction reaction-funny"></div>
-      <div className="reaction reaction-anger"></div>
-      <div className="reaction reaction-sad"></div>
+                } 
+      ></div>
+      <div className="reaction reaction-love" onClick={(e) => {
+                    e.preventDefault();
+                    saveLike(e, 1)
+                      
+                    }
+                } 
+      ></div>
+      <div className="reaction reaction-important" onClick={(e) => {
+                    e.preventDefault();
+                    saveLike(e, 1)
+                      
+                    }
+                } 
+      ></div>
+      <div className="reaction reaction-funny" onClick={(e) => {
+                    e.preventDefault();
+                    saveLike(e, 1)
+                      
+                    }
+                } 
+></div>
+      <div className="reaction reaction-anger" onClick={(e) => {
+                    e.preventDefault();
+                    saveLike(e, 1)
+                      
+                    }
+                } 
+></div>
+      <div className="reaction reaction-sad" onClick={(e) => {
+                    e.preventDefault();
+                    saveLike(e, 1)
+                      
+                    }
+                } 
+></div>
     </div>
   );
 }
