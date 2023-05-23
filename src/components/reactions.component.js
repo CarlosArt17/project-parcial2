3ad27c7e5b32945288874299a6508ca603a88ef4
SrCarlangas69 <zs20006730@estudiantes.uv.mx>
2023-05-23 10:37:51 -0600

    update

--- src/components/reactions.component.js
+++ src/components/reactions.component.js
@@ -1,67 +1,126 @@
-import React from "react";
-import '../styles/photo.css'
-import kafkaService from "../services/kafka.service";
+import React, { Component } from 'react';
+import kafkaService from '../services/kafka.service';
 
-function ReactionsComponent() {
-  function saveLike(e, status) {
+class ReactionsComponet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        likeCount: 0,
+        loveCount: 0,
+        laughCount: 0,
+        cryCount: 0,
+        wowCount: 0,
+        angryCount: 0
+    };
+  }
+
+  saveLike(e, status) {
   
-     let data = {
-       id: 0,
-       status: status
-     };
+    let data = {
+      id: 0,
+      status: status
+    };
   
-     console.log(JSON.stringify(data));
+    console.log(JSON.stringify(data));
   
-     kafkaService.reaction("i-love-adsoftsito");
-     e.preventDefault();
+    kafkaService.reaction("i-love-adsoftsito");
+    e.preventDefault();
  }
 
-  return (
-    <div className="reactions">
-      <div className="reaction reaction-like" onClick={(e) => {
-                    e.preventDefault();
-                    saveLike(e, 1)
-                      
-                    }
-                } 
-      ></div>
-      <div className="reaction reaction-love" onClick={(e) => {
-                    e.preventDefault();
-                    saveLike(e, 1)
-                      
-                    }
-                } 
-      ></div>
-      <div className="reaction reaction-important" onClick={(e) => {
-                    e.preventDefault();
-                    saveLike(e, 1)
-                      
-                    }
-                } 
-      ></div>
-      <div className="reaction reaction-funny" onClick={(e) => {
-                    e.preventDefault();
-                    saveLike(e, 1)
-                      
-                    }
-                } 
-></div>
-      <div className="reaction reaction-anger" onClick={(e) => {
-                    e.preventDefault();
-                    saveLike(e, 1)
-                      
-                    }
-                } 
-></div>
-      <div className="reaction reaction-sad" onClick={(e) => {
-                    e.preventDefault();
-                    saveLike(e, 1)
-                      
-                    }
-                } 
-></div>
-    </div>
-  );
+  
+
+  handleLikeClick = () => {
+    this.setState({ likeCount: this.state.likeCount + 1 });
+  };
+
+  handleLoveClick = () => {
+    this.setState({ loveCount: this.state.loveCount + 1 });
+  };
+
+  handleLaughClick = () => {
+    this.setState({ laughCount: this.state.laughCount + 1 });
+  };
+
+  handleCryClick = () => {
+    this.setState({ cryCount: this.state.cryCount + 1 });
+  };
+
+  handleWowClick = () => {
+    this.setState({ wowCount: this.state.wowCount + 1 });
+  };
+
+  handleAngryClick = () => {
+    this.setState({ angryCount: this.state.angryCount + 1 });
+  };
+
+  render() {
+    const { likeCount, loveCount, laughCount, cryCount , wowCount, angryCount} = this.state;
+
+    return (
+      <div className="reactions">
+        <button className='reaccion' onClick={(e) => {
+          this.handleLikeClick();
+          e.preventDefault();
+          this.saveLike(e, 1);
+        }}>
+          <span role="img" aria-label="like">
+            👍
+          </span>
+          <span className="count">{likeCount}</span>
+        </button>
+        <button className='reaccion' onClick={(e) => {
+          this.handleLoveClick();
+          e.preventDefault();
+          this.saveLike(e, 1);
+        }}>
+          <span role="img" aria-label="love">
+            ❤️
+          </span>
+          <span className="count">{loveCount}</span>
+        </button>
+        <button className='reaccion' onClick={(e) => {
+          this.handleLaughClick();
+          e.preventDefault();
+          this.saveLike(e, 1);
+        }}>
+          <span role="img" aria-label="laugh">
+            😂
+          </span>
+          <span className="count">{laughCount}</span>
+        </button>
+        <button className='reaccion' onClick={(e) => {
+          this.handleCryClick();
+          e.preventDefault();
+          this.saveLike(e, 1);
+        }}>
+          <span role="img" aria-label="cry">
+            😢
+          </span>
+          <span className="count">{cryCount}</span>
+        </button>
+        <button className='reaccion' onClick={(e) => {
+          this.handleWowClick();
+          e.preventDefault();
+          this.saveLike(e, 1);
+        }}>
+          <span role="img" aria-label="wow">
+            😮
+          </span>
+          <span className="count">{wowCount}</span>
+        </button>
+        <button className='reaccion' onClick={(e) => {
+          this.handleAngryClick();
+          e.preventDefault();
+          this.saveLike(e, 1);
+        }}>
+          <span role="img" aria-label="angry">
+            😠
+          </span>
+          <span className="count">{angryCount}</span>
+        </button>
+      </div>
+    );
+  }
 }
 
-export default ReactionsComponent;
+export default ReactionsComponet;
