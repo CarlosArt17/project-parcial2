8f1f6d044d39b573045c67d2e5317c366654bc94
SrCarlangas69 <zs20006730@estudiantes.uv.mx>
2023-05-23 10:01:37 -0600

    kafka services

--- src/components/reactions.component.js
+++ src/components/reactions.component.js
@@ -1,10 +1,33 @@
 import React from "react";
 import '../styles/photo.css'
 
+function saveLike(e, status) { 
+  let data = {
+    id: 0,
+    status: status
+  };
+  console.log(JSON.stringify(data));
+  KafkaService.reaction("i-love-adsoftsito");
+  e.preventDefault();
+}
+
+
+
+
+
 function ReactionsComponent() {
   return (
     <div className="reactions">
-      <div className="reaction reaction-like"></div>
+      <div className="reaction reaction-like">
+      <button onClick={(e) => {
+                    e.preventDefault();
+                    saveLike(e, 1)
+                      
+                    }
+                } >
+                 Love
+              </button>
+    </div>
       <div className="reaction reaction-love"></div>
       <div className="reaction reaction-important"></div>
       <div className="reaction reaction-funny"></div>
